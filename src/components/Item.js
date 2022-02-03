32fb32f3a4acb5ae12a5b724cdcff00903e05d0d
dnjimenez <dnjimenez10@gmail.com>
2022-02-02 23:10:07 -0300

    Contenido Desafio 5

---
+++ src/components/Item.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card, Button} from 'react-bootstrap';
+
+const Item = ({data}) => {
+    return <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={data.img}/>
+        <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>
+                {data.description}
+            </Card.Text>
+        <Button variant="dark">Comprar</Button>
+    </Card.Body>
+    </Card>
+};
+export default Item;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import ItemCount from "./ItemCount";
+import {React, useState, useEffect} from 'react';
+import './ItemListContainer.css';
+import Item from "./Item"
+import charmander from '../img/itemImages/charmander.png'
+import bulbasaur from '../img/itemImages/bulbasaur.png'
+import squirtle from '../img/itemImages/squirtle.png'
+import pikachu from '../img/itemImages/pikachu.png'
 
-const ItemListContainer = (props) => {
-    return <div>
-        {props.message}
-        <ItemCount/>
-    </div>
+const ItemListContainer = () => {
+    
+    const listItems = [
+        {
+            id:"item1",
+            name:"Charmander",
+            description:"Soy un Charmander",
+            stock:1,
+            img: charmander
+        },
+        {
+            id:"item2",
+            name:"Bulbasaur",
+            description:"Soy un Bulbasaur",
+            stock:1,
+            img: bulbasaur
+        },
+        {
+            id:"item3",
+            name:"Squirtle",
+            description:"Soy un Squirtle",
+            stock:1,
+            img: squirtle
+        },
+        {
+            id:"item4",
+            name:"Pikachu",
+            description:"Soy un Pikachu",
+            stock:1,
+            img: pikachu
+        }
+    ]
+
+    return (
+		<div class='itemListContainer'>
+			{listItems.map((item) => {
+                // console.log(item)
+				return (
+					<div key={item.id}>
+						<Item data={item}/>
+					</div>
+				);
+			})}
+		</div>
+	);
+    
 };
 export default ItemListContainer;
